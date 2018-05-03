65969703ae7a0375f2795866a96248d270a7167e
hakbar0 <haseeb2000@hotmail.com>
2018-05-03 17:26:45 +0100

    Initial templated made.

--- pokemon-data/src/App.js
+++ pokemon-data/src/App.js
@@ -3,23 +3,32 @@ import './App.css';
 import axios from 'axios';
 
 class App extends Component {
+
+  state = {
+    pokemon: ""
+  }
+
   componentDidMount() {
-    // axios.get("http://pokeapi.co/api/v2/pokemon/")
-    // .then(res => {
-    //   console.log(res.data.results);
-    // })
+    axios.get("http://pokeapi.co/api/v2/pokemon/")
+      .then(res => { this.setState({ pokemon: res.data.results }) })
   }
 
   render() {
     return (
-
       <div className="App">
-        <div className = "Header"> </div>
-        <div className = "Content">
-        <div className = "One"> </div>
-        <div className = "One"> </div>
-        <div className = "One"> </div>
-        <div className = "One"> </div>
+        <div className="Header"></div>
+        <div className="Content">
+
+          {this.state.pokemon ?
+            this.state.pokemon.map(function (pokemon, index) {
+              return (
+                <div className="One">
+                  <h2>{pokemon.name.toUpperCase()}</h2>
+                  <img src=  {`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`} />
+                </div>
+              )
+            }, this) : "loading"
+          }
         </div>
       </div>
     );
