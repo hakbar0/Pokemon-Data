df5095b7900c5ca5515a676cd321c9dd7ea641d8
hakbar0 <haseeb2000@hotmail.com>
2018-05-03 16:51:32 +0100

    Setup a initial template.

--- pokemon-data/src/App.js
+++ pokemon-data/src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  componentDidMount() {
+    // axios.get("http://pokeapi.co/api/v2/pokemon/")
+    // .then(res => {
+    //   console.log(res.data.results);
+    // })
+  }
+
   render() {
     return (
+
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className = "Header"> </div>
+        <div className = "Content">
+        <div className = "One"> </div>
+        <div className = "One"> </div>
+        <div className = "One"> </div>
+        <div className = "One"> </div>
+        </div>
       </div>
     );
   }
