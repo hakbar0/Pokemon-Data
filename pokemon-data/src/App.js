6a014b7715bf2f9dd5b97ed4e89df4292831a733
hakbar0 <haseeb2000@hotmail.com>
2018-05-04 14:40:14 +0100

    setup the overlay.

--- pokemon-data/src/App.js
+++ pokemon-data/src/App.js
@@ -19,15 +19,25 @@ class App extends Component {
         <div className="Header"></div>
         <div className="Content">
 
+
           {this.state.pokemon ?
             this.state.pokemon.map(function (pokemon, index) {
               return (
                 <div className="One">
                   <h2>{pokemon.name.toUpperCase()}</h2>
-                  <img src=  {`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`} />
+                  <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`} alt="pokemon" />
+                 
+                  <div id="myModal" className="modal1">
+                    <div className="modal-content1">
+                      <span className="close1">&times;</span>
+                      <p>Some text in the Modal..</p>
+                    </div>
+
+                  </div>
+
                 </div>
               )
-            }, this) : "loading"
+            }, this) : <img src={`https://media.giphy.com/media/52qtwCtj9OLTi/giphy.gif`} alt="loading" />
           }
         </div>
       </div>
